3fe632ca6fcf59e62f44878f64895ae22f212770
Marco Kellershoff <1384938+gorillamoe@users.noreply.github.com>
2025-12-08 14:51:57 +0100

    feat(api): add `clear` and `clear_all` functions (#11)
    - `require("kikao").clear()` - Clears the current project's session file.
      Also closes all buffers.
    - `require("kikao").clear_all()` - Clears all session files.
      Also closes all buffers.
    - `require("kikao").version()` - Returns the version info.
    
    Also fix an issue where the the session kept the last opened
    file info, instead of just wiping all session data,
    when you have no buffers left.

--- lua/kikao/init.lua
+++ lua/kikao/init.lua
@@ -1,12 +1,24 @@
 local Globals = require("kikao.globals")
-local Config = require("kikao.config")
 local Logger = require("kikao.logger")
+local Api = require("kikao.api")
 local M = {}
 
+---Sets up Kikao with the provided configuration.
 M.setup = function(config)
-  Config.setup(config)
+  Api.setup(config)
 end
 
+---Clears cached data for the current project and closes all Kikao buffers.
+M.clear = function()
+  Api.clear()
+end
+
+---Clears all cached data and closes all Kikao buffers.
+M.clear_all = function()
+  Api.clear_all()
+end
+
+---Prints the current Kikao version and Neovim version to the log.
 M.version = function()
   local neovim_version = vim.fn.execute("version") or "Unknown"
   Logger.info("Kikao version: " .. Globals.VERSION .. "\n\n" .. "Neovim version: " .. neovim_version)
