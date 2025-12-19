4f74606c274a6884d205e828ce6ebf8fb2e7e4e5
Marco Kellershoff <marco@mistweaverco.com>
2025-12-19 13:01:58 +0100

    feat(api): add get and set value for current workspace api methods

--- lua/kikao/api.lua
+++ lua/kikao/api.lua
@@ -7,7 +7,7 @@ end
 
 ---Clears cached data for the current project and closes all Kikao buffers.
 M.clear = function()
-  local utils = require("lua.kikao.config.utils")
+  local utils = require("kikao.config.utils")
   local project_root = utils.get_root_dir(Config.options.project_dir_matchers)
   local project_cache_dir = utils.get_cache_dir(project_root)
   if project_cache_dir and utils.file_exists(project_cache_dir) then
@@ -20,7 +20,7 @@ end
 
 ---Clears all cached data and closes all Kikao buffers.
 M.clear_all = function()
-  local utils = require("lua.kikao.config.utils")
+  local utils = require("kikao.config.utils")
   local all_cache_dir = utils.get_cache_dir()
   if all_cache_dir and utils.file_exists(all_cache_dir) then
     vim.fn.delete(all_cache_dir, "rf")
@@ -28,6 +28,41 @@ M.clear_all = function()
   for _, buf in pairs(vim.fn.getbufinfo()) do
     vim.api.nvim_buf_delete(buf.bufnr, { force = true })
   end
+end
+
+---@class KikaoAPIGetValueOpts
+---@field key string The key of the option to set.
+
+---Gets the value of a workspace option stored in the maybe existing metadata.
+---@param opts KikaoAPIGetValueOpts
+---@return any|nil The value of the option, or nil if not found.
+M.get_value = function(opts)
+  local utils = require("kikao.config.utils")
+  local project_root = utils.get_root_dir(Config.options.project_dir_matchers)
+  if not project_root then
+    return nil
+  end
+  local metadata = utils.get_project_metadata(project_root)
+  if not metadata then
+    return nil
+  end
+  return metadata[opts.key]
+end
+
+---@class KikaoAPISetValueOpts
+---@field key string The key of the option to set.
+---@field value any The value to set the option to.
+
+---Sets the value of a workspace option stored in the maybe existing metadata.
+---@param opts KikaoAPISetValueOpts
+---@return boolean success
+M.set_value = function(opts)
+  local utils = require("kikao.config.utils")
+  local project_root = utils.get_root_dir(Config.options.project_dir_matchers)
+  if not project_root then
+    return false
+  end
+  return utils.write_project_metadata(project_root, { [opts.key] = opts.value })
 end
 
 return M
